21270b66d660966b92683b4e1e7606311b8c5522
shnooki <82466624+vikishptra@users.noreply.github.com>
2021-11-14 20:12:15 +0700

    Create loop2.c

---
+++ loop2.c
@@ -0,0 +1,27 @@
+main()
+{
+
+    int a,b,n,sum,an,i;
+
+    printf("Masukkan Nilai a= ");
+    scanf("%d", &a);
+
+    printf("Masukkan Nilai b= ");
+    scanf("%d", &b);
+
+    printf("Masukkan Nilai n= ");
+    scanf("%d", &n);
+
+    sum = 0;
+    for (i=0; i<5; i++)
+    {
+        an = a + b * i;
+        sum = sum + an;
+
+        printf("%3d", an);
+        
+
+    }
+
+    printf("\n Jumlah = %3d", sum);
+}
